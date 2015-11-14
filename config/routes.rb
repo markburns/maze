25bdf48542f6be34a89bc922f03f84aa010722ae
Mark Burns <markthedeveloper@gmail.com>
2015-11-14 18:00:02 +0900

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :mazes, only: [:new, :create, :show]
+  root "mazes#new"
 end
--- spec/integration/new_maze_spec.rb
+++ spec/integration/new_maze_spec.rb
@@ -2,9 +2,8 @@ require "rails_helper"
 
 describe "New Maze", type: :feature do
   it do
-    visit new_maze_path
+    visit root_path
 
-    save_and_open_page
     click_on "12x12 Maze"
 
     expect(page).to have_content "←"
